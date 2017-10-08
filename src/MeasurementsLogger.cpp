0d0a10e712eb9d8cd422d7908b067265256788b4
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2017-10-08 15:24:46 +0200

    Add MeasurementsLogger module

---
+++ src/MeasurementsLogger.cpp
@@ -0,0 +1,85 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#include <cassert>
+#include "inet/common/INETDefs.h"
+
+#include "MeasurementsLogger.h"
+
+namespace smile {
+
+Define_Module(MeasurementsLogger);
+
+MeasurementsLogger::~MeasurementsLogger()
+{
+  logFile.flush();
+}
+
+void MeasurementsLogger::initialize(int stage)
+{
+  cModule::initialize(stage);
+  if (stage == inet::INITSTAGE_LOCAL) {
+  }
+}
+
+int MeasurementsLogger::numInitStages() const
+{
+  return inet::INITSTAGE_LOCAL + 1;
+}
+
+void MeasurementsLogger::openFile()
+{
+  const auto& directoryPathParameter = par("directoryPath");
+  const auto& fileNameParameter = par("fileName");
+  const auto& formatParameter = par("format");
+  const auto& overwriteParameter = par("overwrite");
+
+  assert(directoryPathParameter.getType() == cPar::STRING);
+  assert(fileNameParameter.getType() == cPar::STRING);
+  assert(formatParameter.getType() == cPar::STRING);
+  assert(overwriteParameter.getType() == cPar::BOOL);
+
+  const auto directoryPath = directoryPathParameter.stdstringValue();
+  if (directoryPath.empty()) {
+    EV_ERROR << "MeasurementsLogger.directoryPath cannot be empty" << endl;
+    endSimulation();
+  }
+
+  const auto fileName = fileNameParameter.stdstringValue();
+  if (fileName.empty()) {
+    EV_ERROR << "MeasurementsLogger.fileName cannot be empty" << endl;
+    endSimulation();
+  }
+
+  const auto format = formatParameter.stdstringValue();
+  if (format.empty()) {
+    EV_ERROR << "MeasurementsLogger.format cannot be empty" << endl;
+    endSimulation();
+  }
+  if (format != "CSV") {
+    EV_ERROR << "MeasurementsLogger supports only CSV format" << endl;
+    endSimulation();
+  }
+
+  const auto overwrite = overwriteParameter.boolValue();
+
+  std::ostringstream filePath;
+  filePath << directoryPath << '/' << fileName;
+  const auto mode = std::ios_base::out | (overwrite ? std::ios_base::trunc : std::ios_base::ate);
+
+  logFile = std::fstream(filePath.str(), mode);
+}
+
+}  // namespace smile
