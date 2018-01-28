bf36030f7aa8e4911961cdb2ba6539330c873a4c
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2018-01-28 14:42:08 +0100

    Add two methods to smile.analysis
    absolute_position_error_histogram()
    absolute_position_error_surface()

--- python/smile/analysis.py
+++ python/smile/analysis.py
@@ -13,23 +13,77 @@
 # along with this program.  If not, see http:#www.gnu.org/licenses/.
 #
 
-import numpy
-import matplotlib.pyplot as pyplot
-from smile.helpers import PositionDimensions
+import numpy as np
+import matplotlib.pyplot as plt
+from smile.results import Results
 
 
-def absolute_position_error(result):
-    if result.position_dimensions is PositionDimensions.TWO_D:
-        coordinates = (0, 1)
-    elif result.position_dimensions is PositionDimensions.THREE_D:
-        coordinates = (0, 1, 2)
+def _determine_dimensions(results):
+    unique_dimensions = np.unique(results[:, Results.POSITION_DIMENSIONS])
+    if len(unique_dimensions) is not 1:
+        raise ValueError('Cannot process results with different position dimensions (2D and 3D)')
 
-    true_position = (result.begin_true_position[:, coordinates] + result.end_true_position[:, coordinates]) / 2
-    position_error = numpy.abs(numpy.linalg.norm(true_position - result.position[:, coordinates], axis=1))
+    if results[0, Results.POSITION_DIMENSIONS] == 2:
+        return Results.POSITION_2D, Results.BEGIN_TRUE_POSITION_2D,  Results.END_TRUE_POSITION_2D
+    else:
+        return Results.POSITION_3D, Results.BEGIN_TRUE_POSITION_3D,  Results.END_TRUE_POSITION_3D
 
-    pyplot.hist(position_error)
-    pyplot.title('Histogram of absolute error values')
-    pyplot.xlabel('Position error [m]')
-    pyplot.ylabel('Number of localization results')
-    pyplot.grid(True)
-    pyplot.show()
+
+def absolute_position_error_histogram(results):
+    position_coordinates, begin_position_coordinates, end_position_coordinates = _determine_dimensions(results)
+    # Mobile node cloud move during localization procedure
+    true_position = (results[:, begin_position_coordinates] + results[:, end_position_coordinates]) / 2
+    position_errors = np.abs(np.linalg.norm(true_position - results[:, position_coordinates], axis=1))
+
+    plt.hist(position_errors)
+    plt.title('Histogram of absolute error values')
+    plt.xlabel('Position error [m]')
+    plt.ylabel('Number of localization results')
+    plt.grid(True)
+    plt.show()
+
+
+def absolute_position_error_surface(results):
+    position_coordinates, begin_position_coordinates, end_position_coordinates = _determine_dimensions(results)
+
+    # We want to have single result per mobile node
+    unique_mac_addresses = np.unique(results[:, Results.MAC_ADDRESS])
+    unique_results = Results.create_array(len(unique_mac_addresses))
+
+    for i in range(0, len(unique_mac_addresses)):
+        mac_address = unique_mac_addresses[i]
+        tmp_results = results[results[:, Results.MAC_ADDRESS] == mac_address]
+
+        unique_results[i, :] = tmp_results[0, :]
+        # TODO Implement more ways of "squeezing" multiple results into one
+        unique_results[i, begin_position_coordinates] = np.mean(tmp_results[:, begin_position_coordinates], axis=0)
+        unique_results[i, end_position_coordinates] = np.mean(tmp_results[:, end_position_coordinates], axis=0)
+        unique_results[i, position_coordinates] = np.mean(tmp_results[:, position_coordinates], axis=0)
+
+    # Mobile node cloud move during localization procedure
+    true_positions = (unique_results[:, begin_position_coordinates] + unique_results[:, end_position_coordinates]) / 2
+    position_errors = np.abs(np.linalg.norm(true_positions - unique_results[:, position_coordinates], axis=1))
+
+    x, y = np.meshgrid(np.unique(true_positions[:, 0]), np.unique(true_positions[:, 1]), indexing='xy')
+    z = np.zeros(x.shape)
+
+    # Assign position errors (z) to correct node coordinates (x, y)
+    for i in range(0, len(true_positions)):
+        tmp_true_position = true_positions[i, :]
+        tmp_position_error = position_errors[i]
+
+        tmp_z_indices = np.where(np.logical_and(x == tmp_true_position[0], y == tmp_true_position[1]))
+        z[tmp_z_indices] = tmp_position_error
+
+    plt.pcolormesh(x, y, z)
+    y_min, y_max = plt.ylim()
+    plt.ylim(y_max, y_min)
+    axis = plt.gca()
+    axis.xaxis.set_ticks_position('top')
+    axis.xaxis.set_label_position('top')
+    plt.colorbar().set_label('Error value [m]')
+    plt.grid()
+    plt.title('Map of absolute position errors', y=-0.1)
+    plt.xlabel('X [m]')
+    plt.ylabel('Y [m]')
+    plt.show()
