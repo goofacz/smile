96763b68b50d67415daf6dc1bb47a21b771881b9
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2018-02-01 21:48:03 +0100

    Add smile.analysis.absolute_position_error_histogram UT

--- python/tests/test_analysis.py
+++ python/tests/test_analysis.py
@@ -47,6 +47,31 @@ class TestAnalysis(unittest.TestCase):
         self.assertEqual(15, position_errors[1])
         self.assertAlmostEqual(sqrt(2 * 10**2) - sqrt(2 * 6**2), position_errors[2], places=10)
 
+    def test_absolute_position_error_histogram(self):
+        results = Results.create_array(3, 2)
+        results[0, Results.BEGIN_TRUE_POSITION_2D] = (0, 0)
+        results[0, Results.POSITION_2D] = (15, 0)
+        results[0, Results.END_TRUE_POSITION_2D] = (20, 0)
+
+        results[1, Results.BEGIN_TRUE_POSITION_2D] = (0, 0)
+        results[1, Results.POSITION_2D] = (0, 5)
+        results[1, Results.END_TRUE_POSITION_2D] = (0, 40)
+
+        results[2, Results.BEGIN_TRUE_POSITION_2D] = (0, 0)
+        results[2, Results.POSITION_2D] = (6, 6)
+        results[2, Results.END_TRUE_POSITION_2D] = (20, 20)
+
+        true_position, position_errors = absolute_position_error_histogram(results, return_intermediate_results=True)
+        self.assertTupleEqual((3, 2), true_position.shape)
+        np.testing.assert_equal((10, 0), true_position[0, :])
+        np.testing.assert_equal((0, 20), true_position[1, :])
+        np.testing.assert_equal((10, 10), true_position[2, :])
+
+        self.assertTupleEqual((3, ), position_errors.shape)
+        self.assertEqual(5, position_errors[0])
+        self.assertEqual(15, position_errors[1])
+        self.assertAlmostEqual(sqrt(2 * 10**2) - sqrt(2 * 6**2), position_errors[2], places=10)
+
 
 if __name__ == '__main__':
     unittest.main()
