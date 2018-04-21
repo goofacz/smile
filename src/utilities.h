6f1370389d443e8859ae5463b93e21b3de5ce056
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2018-04-21 20:46:32 +0200

    Introduce SequenceNumberGenerator utility class

--- src/utilities.h
+++ src/utilities.h
@@ -16,7 +16,9 @@
 #pragma once
 
 #include <omnetpp.h>
+#include <limits>
 #include <memory>
+#include <type_traits>
 
 namespace smile {
 
@@ -25,6 +27,33 @@ auto dynamic_unique_ptr_cast(std::unique_ptr<InputType> object)
 {
   omnetpp::check_and_cast<OutputType*>(object.get());
   return std::unique_ptr<OutputType>(static_cast<OutputType*>(object.release()));
+};
+
+template <typename T>
+class SequenceNumberGenerator final
+{
+  static_assert(std::is_integral<T>::value, "T has to be an integral type");
+
+ public:
+  SequenceNumberGenerator() = default;
+  SequenceNumberGenerator(const SequenceNumberGenerator&) = default;
+  SequenceNumberGenerator(SequenceNumberGenerator&&) = default;
+  ~SequenceNumberGenerator() = default;
+
+  SequenceNumberGenerator& operator=(const SequenceNumberGenerator&) = default;
+  SequenceNumberGenerator& operator=(SequenceNumberGenerator&&) = default;
+
+  T generateNext()
+  {
+    if (current == std::numeric_limits<T>::max()) {
+      throw omnetpp::cRuntimeError{"maximum sequence number reached"};
+    }
+
+    return current++;
+  }
+
+ private:
+  T current{0};
 };
 
 }  // namespace smile
