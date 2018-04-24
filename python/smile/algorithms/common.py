f9cdeb0a8d21c2db12a1be4102b566395aa67eb0
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2018-04-24 22:04:33 +0200

    Implement sample ToF and TDoA data generators to algorithms.common

--- python/smile/algorithms/common.py
+++ python/smile/algorithms/common.py
@@ -13,7 +13,17 @@
 # along with this program.  If not, see http:#www.gnu.org/licenses/.
 #
 
+import itertools
 import numpy as np
+import scipy.constants as sc
+
+
+def __compute_distance(anchor_coordinates, mobile_coordinates):
+    return np.abs(np.linalg.norm(mobile_coordinates - anchor_coordinates))
+
+
+def __generate_distances(anchors_coordinates, mobile_coordinates):
+    return np.apply_along_axis(__compute_distance, 1, anchors_coordinates, mobile_coordinates)
 
 
 def sort_distances(coordinates, distances):
@@ -26,3 +36,19 @@ def sort_distances(coordinates, distances):
 
     indices = np.argsort(distances)
     return coordinates[indices, :], distances[indices]
+
+
+def generate_tof_measurements(anchor_coordinates, grid_size, grid_gap):
+    assert (sc.unit('speed of light in vacuum') == 'm s^-1')
+    c = sc.value('speed of light in vacuum')
+    c = c * 1e-12  # m/s -> m/ps
+
+    for mobile_coordinates in itertools.product(range(0, grid_size + 1, grid_gap), repeat=2):
+        measurements = __generate_distances(anchor_coordinates, mobile_coordinates)
+        yield np.asanyarray(mobile_coordinates), measurements * c
+
+
+def generate_tdoa_measurements(anchor_coordinates, grid_size, grid_gap):
+    for mobile_coordinates, measurements in generate_tof_measurements(anchor_coordinates, grid_size, grid_gap):
+        yield mobile_coordinates, measurements - min(measurements)
+
--- python/tests/test_algorithms_common.py
+++ python/tests/test_algorithms_common.py
@@ -55,7 +55,6 @@ class TestSortDistances(unittest.TestCase):
 
     def test_invalid_coordinates(self):
         distances = np.asarray([4, 2, 3])
-
         coordinates_1d = np.asarray([4, 2, 3])
         with self.assertRaises(AttributeError):
             sort_distances(coordinates_1d, distances)
@@ -63,6 +62,58 @@ class TestSortDistances(unittest.TestCase):
         coordinates_4d = np.asarray([[40, 40, 44, 44], [20, 20, 22, 22], [30, 30, 33, 33]])
         with self.assertRaises(AttributeError):
             sort_distances(coordinates_4d, distances)
+
+
+class TestGenerateTofMeasurements(unittest.TestCase):
+    def test_small_grid(self):
+        anchor_coordinates = (np.asanyarray((0, 0)), np.asanyarray((0, 2)),
+                              np.asanyarray((2, 0)), np.asanyarray((2, 1)))
+        grid_size = 2
+        grid_gap = 1
+
+        reference_mobile_positions = (np.asanyarray((0, 0)), np.asanyarray((0, 1)), np.asanyarray((0, 2)),
+                                      np.asanyarray((1, 0)), np.asanyarray((1, 1)), np.asanyarray((1, 2)),
+                                      np.asanyarray((2, 0)), np.asanyarray((2, 1)), np.asanyarray((2, 2)))
+
+        reference_tof_values = (np.asanyarray((0, 0.00059958, 0.00059958, 0.00067036)),
+                                np.asanyarray((0.00029979, 0.00029979, 0.00067036, 0.00059958)),
+                                np.asanyarray((0.00059958, 0, 0.00084794, 0.00067036)),
+                                np.asanyarray((0.00029979, 0.00067036, 0.00029979, 0.00042397)),
+                                np.asanyarray((0.00042397, 0.00042397, 0.00042397, 0.00029979)),
+                                np.asanyarray((0.00067036, 0.00029979, 0.00067036, 0.00042397)),
+                                np.asanyarray((0.00059958, 0.00084794, 0, 0.00029979)),
+                                np.asanyarray((0.00067036, 0.00067036, 0.00029979, 0)),
+                                np.asanyarray((0.00084794, 0.00059958, 0.00059958, 0.00029979)))
+
+        mobile_positions, tof_values = zip(*generate_tof_measurements(anchor_coordinates, grid_size, grid_gap))
+        np.testing.assert_equal(mobile_positions, reference_mobile_positions)
+        np.testing.assert_almost_equal(tof_values, reference_tof_values)
+
+
+class TestGenerateTdoaMeasurements(unittest.TestCase):
+    def test_small_grid(self):
+        anchor_coordinates = (np.asanyarray((0, 0)), np.asanyarray((0, 2)),
+                              np.asanyarray((2, 0)), np.asanyarray((2, 1)))
+        grid_size = 2
+        grid_gap = 1
+
+        reference_mobile_positions = (np.asanyarray((0, 0)), np.asanyarray((0, 1)), np.asanyarray((0, 2)),
+                                      np.asanyarray((1, 0)), np.asanyarray((1, 1)), np.asanyarray((1, 2)),
+                                      np.asanyarray((2, 0)), np.asanyarray((2, 1)), np.asanyarray((2, 2)))
+
+        reference_tdoa_values = (np.asanyarray((0, 0.00059958, 0.00059958, 0.00067036)),
+                                 np.asanyarray((0, 0, 0.00037056, 0.00029979)),
+                                 np.asanyarray((0.00059958, 0, 0.00084794, 0.00067036)),
+                                 np.asanyarray((0, 0.00037056, 0, 0.00012418)),
+                                 np.asanyarray((0.00012418, 0.00012418, 0.00012418, 0)),
+                                 np.asanyarray((0.00037056, 0, 0.00037056, 0.00012418)),
+                                 np.asanyarray((0.00059958, 0.00084794, 0, 0.00029979)),
+                                 np.asanyarray((0.00067036, 0.00067036, 0.00029979, 0)),
+                                 np.asanyarray((0.00054815, 0.00029979, 0.00029979, 0)))
+
+        mobile_positions, tdoa_values = zip(*generate_tdoa_measurements(anchor_coordinates, grid_size, grid_gap))
+        np.testing.assert_equal(mobile_positions, reference_mobile_positions)
+        np.testing.assert_almost_equal(tdoa_values, reference_tdoa_values)
 
 
 if __name__ == '__main__':
