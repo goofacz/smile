33769298e9d85474c662514392e2d1a92616537c
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2018-04-25 22:49:45 +0200

    Simplify algorithms.tdoa UTs

--- python/tests/test_algorithms_tdoa.py
+++ python/tests/test_algorithms_tdoa.py
@@ -19,159 +19,61 @@ from smile.algorithms.common import *
 from smile.algorithms.tdoa import *
 
 
-# Test scenarios contains following information
-# - Anchors' coordinates in 2D
-# - True tag position in 2D
-# - True differences in distances between tag and Anchors no. 2 and 3 relative to distance between tag and Anchor no.1
-
-
-def generate_scenario_data(mobile_position, anchors_number):
-    if anchors_number == 3:
-        coordinates = np.asarray(((0, 0), (10, 0), (10, 10)))
-    elif anchors_number == 4:
-        coordinates = np.asarray(((0, 0), (10, 0), (10, 10), (0, 10)))
-    else:
-        raise ValueError('Unsupported value of anchors_number')
-
-    distances = np.apply_along_axis(lambda anchor_position: np.abs(np.linalg.norm(mobile_position - anchor_position)),
-                                    1, coordinates)
-    distances -= distances[0]
-
-    return coordinates, distances, mobile_position
-
-
-def scenario_1(anchors_number=3):
-    return generate_scenario_data((6, 4), anchors_number)
-
-
-def scenario_2(anchors_number=3):
-    return generate_scenario_data((5, 5), anchors_number)
-
-
-def scenario_3(anchors_number=3):
-    return generate_scenario_data((7, 6), anchors_number)
-
-
-def scenario_4(anchors_number=3):
-    return generate_scenario_data((8, 2), anchors_number)
-
-
-def scenario_5(anchors_number=3):
-    return generate_scenario_data((8, 8), anchors_number)
-
-
-def scenario_6(anchors_number=3):
-    return generate_scenario_data((0, 0), anchors_number)
-
-
 class TestDoanVesely(unittest.TestCase):
-    def test_scenario_1(self):
-        coordinates, distances, true_mobile_position = scenario_1()
-        position = doan_vesely(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
+    def test_small_grid(self):
+        anchors_coordinates = np.asanyarray(((0, 0),
+                                             (0, 6),
+                                             (6, 6)))
+        grid_size = 6
+        grid_gap = 1
 
-    def test_scenario_2(self):
-        coordinates, distances, true_mobile_position = scenario_2()
-        position = doan_vesely(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
+        # For these positions algorithm fails to compute correct solution
+        skipped_positions = np.asanyarray(((0, 5), (1, 6)))
 
-    def test_scenario_3(self):
-        coordinates, distances, true_mobile_position = scenario_3()
-        position = doan_vesely(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
-
-    def test_scenario_4(self):
-        coordinates, distances, true_mobile_position = scenario_4()
-        position = doan_vesely(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
-
-    def test_scenario_5(self):
-        coordinates, distances, true_mobile_position = scenario_5()
-        position = doan_vesely(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
-
-    def test_scenario_6(self):
-        coordinates, distances, true_mobile_position = scenario_6()
-        position = doan_vesely(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
+        for reference_position, tdoa_distances in generate_tdoa_measurements(anchors_coordinates, grid_size, grid_gap):
+            if np.all(np.isin(reference_position, skipped_positions)):
+                continue
+            sorted_anchors_coordinates, sorted_tdoa_distances = sort_measurements(anchors_coordinates, tdoa_distances)
+            position = doan_vesely(sorted_anchors_coordinates, sorted_tdoa_distances)
+            np.testing.assert_almost_equal(position, reference_position, decimal=7)
 
 
 class TestFang(unittest.TestCase):
-    def test_scenario_1(self):
-        coordinates, distances, true_mobile_position = scenario_1()
-        position = fang(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
+    def test_small_grid(self):
+        anchors_coordinates = np.asanyarray(((0, 6),
+                                             (6, 6),
+                                             (6, 0)))
+        grid_size = 6
+        grid_gap = 1
 
-    def test_scenario_2(self):
-        coordinates, distances, true_mobile_position = scenario_2()
-        with self.assertRaises(ValueError):
-            fang(coordinates, distances)
+        # For these positions algorithm fails to compute correct solution
+        skipped_positions = np.asanyarray(((0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6)))
 
-    def test_scenario_3(self):
-        coordinates, distances, true_mobile_position = scenario_3()
-        position = fang(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
-
-    def test_scenario_4(self):
-        coordinates, distances, true_mobile_position = scenario_4()
-        position = fang(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
-
-    def test_scenario_5(self):
-        coordinates, distances, true_mobile_position = scenario_5()
-        position = fang(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
-
-    def test_scenario_6(self):
-        coordinates, distances, true_mobile_position = scenario_6()
-        position = fang(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
-
-    def test_incorrect_anchors_deployment(self):
-        coordinates = np.asarray(((0, 0),  # Anchor 1
-                                  (10, 50),  # Anchor 2
-                                  (10, 10)))  # Anchor 3
-        distances = np.asarray((1, 2, 3))
-        with self.assertRaises(ValueError):
-            fang(coordinates, distances)
+        for reference_position, tdoa_distances in generate_tdoa_measurements(anchors_coordinates, grid_size, grid_gap):
+            if np.all(np.isin(reference_position, skipped_positions)):
+                continue
+            position = fang(anchors_coordinates, tdoa_distances)
+            np.testing.assert_almost_equal(position, reference_position, decimal=7)
 
 
 class TestChanHo(unittest.TestCase):
-    def test_scenario_1(self):
-        coordinates, distances, true_mobile_position = scenario_1(anchors_number=4)
-        coordinates, distances = sort_measurements(coordinates, distances)
-        position = chan_ho(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
+    def test_small_grid(self):
+        anchors_coordinates = np.asanyarray(((0, 0),
+                                             (0, 6),
+                                             (6, 0),
+                                             (6, 6)))
+        grid_size = 6
+        grid_gap = 1
 
-    def test_scenario_2(self):
-        coordinates, distances, true_mobile_position = scenario_2(anchors_number=4)
-        coordinates, distances = sort_measurements(coordinates, distances)
-        with self.assertRaises(np.linalg.linalg.LinAlgError):
-            chan_ho(coordinates, distances)
+        # For these positions algorithm fails to compute correct solution
+        skipped_positions = np.asanyarray(((0, 3), (1, 3), (2, 3), (4, 3), (5, 3), (6, 3)))
 
-    def test_scenario_3(self):
-        coordinates, distances, true_mobile_position = scenario_3(anchors_number=4)
-        coordinates, distances = sort_measurements(coordinates, distances)
-        position = chan_ho(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
-
-    def test_scenario_4(self):
-        coordinates, distances, true_mobile_position = scenario_4(anchors_number=4)
-        coordinates, distances = sort_measurements(coordinates, distances)
-        position = chan_ho(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
-
-    def test_scenario_5(self):
-        coordinates, distances, true_mobile_position = scenario_5(anchors_number=4)
-        coordinates, distances = sort_measurements(coordinates, distances)
-        position = chan_ho(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
-
-    def test_scenario_6(self):
-        coordinates, distances, true_mobile_position = scenario_6(anchors_number=4)
-        coordinates, distances = sort_measurements(coordinates, distances)
-        position = chan_ho(coordinates, distances)
-        np.testing.assert_almost_equal(position, true_mobile_position, decimal=7)
+        for reference_position, tdoa_distances in generate_tdoa_measurements(anchors_coordinates, grid_size, grid_gap):
+            if np.all(np.isin(reference_position, skipped_positions)):
+                continue
+            sorted_anchors_coordinates, sorted_tdoa_distances = sort_measurements(anchors_coordinates, tdoa_distances)
+            position = chan_ho(sorted_anchors_coordinates, sorted_tdoa_distances)
+            np.testing.assert_almost_equal(position, reference_position, decimal=7)
 
 
 if __name__ == '__main__':
