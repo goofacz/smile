f60fee6aa5bba039e5e65da7c5c249804a2323cf
Tomasz Jankowski <goofacz@users.noreply.github.com>
2018-05-17 20:59:23 +0200

    Add JsonParser platform module (#7)

---
+++ python/tests/test_platform_json_parser.py
@@ -0,0 +1,61 @@
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http:#www.gnu.org/licenses/.
+#
+
+import unittest
+
+from smile.platform.json_parser import *
+
+
+class TestJsonParser(unittest.TestCase):
+    def test_empty_json(self):
+        json = JsonParser('platform_json_parser/empty.json')
+        self.assertIsInstance(json, JsonParser)
+
+    def test_disjoint_jsons(self):
+        json = JsonParser('platform_json_parser/disjoint_top.json')
+
+        self.assertFalse('import' in json.content)
+
+        self.assertTrue('foo' in json.content)
+        self.assertEqual(json.content['foo'], 123)
+
+        self.assertTrue('bar' in json.content)
+        self.assertEqual(json.content['bar'], 456)
+
+    def test_cyclical_jsons(self):
+        with self.assertRaises(ValueError):
+            JsonParser('platform_json_parser/cyclical_import_top.json')
+
+    def test_override_jsons(self):
+        json = JsonParser('platform_json_parser/override_top.json')
+
+        self.assertFalse('import' in json.content)
+
+        self.assertTrue('bottom_only_number' in json.content)
+        self.assertEqual(json.content['bottom_only_number'], 112)
+
+        self.assertTrue('number' in json.content)
+        self.assertEqual(json.content['number'], 789)
+
+        self.assertTrue('text' in json.content)
+        self.assertEqual(json.content['text'], 'def')
+
+        self.assertTrue('object' in json.content)
+        self.assertTrue('collection' in json.content['object'])
+        self.assertSequenceEqual(json.content['object']['collection'], [4, 5, 6])
+
+
+if __name__ == '__main__':
+    unittest.main()
